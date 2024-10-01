ae1a7ec25d4d52ed883093b45cc16b237e977685
hisreal123 <omosgoerge@gmail.com>
2024-10-01 02:46:27 +0100

    task index 6

--- 0x00-ES6_basic/.history/6-string-interpolation_20241001024516.js
+++ 0x00-ES6_basic/.history/6-string-interpolation_20241001024516.js
@@ -2,8 +2,8 @@ export default function getSanFranciscoDescription() {
   const year = 2017;
   const budget = {
     income: '$119,868',
-    gdp: '$154.2 billion',
-    capita: '$178,479',
+    gdp: '$154.2 billion",
+    capita: "$178,479",
   };
 
   return `As of ${year}, it was the seventh-highest income county in the United States\
