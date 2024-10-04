fc30f67f18d43c11828a98ecab49563b4e5d7cbd
hisreal123 <omosgoerge@gmail.com>
2024-10-04 03:00:43 +0100

    PROMISES

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from "./utils.js";
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((responses) => {
+      const [photoResponse, userResponse] = responses;
+      console.log(`${photoResponse.body} ${userResponse.firstName}
+	          ${userResponse.lastName}`);
+    })
+    .catch(() => {
+      console.log("Signup system offline");
+    });
+}
