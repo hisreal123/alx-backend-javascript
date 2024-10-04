e7147e57a9f0db9ccfcefee7b5abc775de9cdd68
hisreal123 <omosgoerge@gmail.com>
2024-10-04 02:35:40 +0100

    2 holberton course

---
+++ 0x02-ES6_classes/.history/100-evcar_20241004023539.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+
+import Car from "./10-car";
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    const Spec = super.constructor[Symbol.species];
+    return new Spec();
+  }
+}
