c4329ff6653b39e2a18502853b4de5f0ff6fb415
hisreal123 <omosgoerge@gmail.com>
2024-10-09 03:17:11 +0100

    welcome to map - 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/.history/10-update_uniq_items_20241009031704.js
+++ 0x03-ES6_data_manipulation/.history/10-update_uniq_items_20241009031704.js
@@ -4,7 +4,7 @@
 
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
-    throw new Error('Cannot process');
+    throw new Error("Cannot process');
   }
   map.forEach((value, key) => {
     if (value === 1) {
