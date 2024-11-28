5f1d280c2b906a4ab18bd48c540e418528ceb690
hisreal123 <omosgoerge@gmail.com>
2024-11-28 13:28:11 +0100

    welcome to sinon

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -16,9 +16,8 @@ describe("sendPaymentRequestToApi", () => {
     const calculateNumberSpy = sinon.spy(Utils);
 
     sendPaymentRequestToApi(100, 20);
-    expect(calculateNumberSpy.calculateNumber.called).to.be.true(1); // check if the function was called
     expect(calculateNumberSpy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true; // check specific arguements it is calld with
+    expect(calculateNumberSpy.calculateNumber.called).to.be.equal(1); // check if the function was called
     calculateNumberSpy.calculateNumber.restore(); // used to revert the behavior of the spy to its original state
-
   });
 });
