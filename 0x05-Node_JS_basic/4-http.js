c19427072f50b10d7d66287421913766464ca7f8
hisreal123 <omosgoerge@gmail.com>
2024-12-01 04:39:38 +0100

    4-up and running

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,20 +1,22 @@
-const http = require("http");
+const http = require('http');
 
-const PORT = 1245;
-const HOST = "localhost";
-const app = http.createServer();
+const PORT = 1245
+const HOST = 'localhost'
+const app = http.createServer()
 
-app.on("request", (_, res) => {
-  const responseText = "Hello Holberton School!";
-
-  res.setHeader("Content-Type", "text/plain");
-  res.setHeader("Content-Length", responseText.length);
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+  res.setHeader({
+    'Content-Type': 'text/plain',
+    'Content-Length': responseText.length
+  })
   res.statusCode = 200;
-  res.write(Buffer.from(responseText));
+  res.write(Buffer.from(responseText))
 });
 
-app.listen(PORT, HOST, () => {
-  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
-});
+app.listen(1245, () => {
+  console.log(`Server is listening on -> http://${HOST}:${PORT}\n`);
+})
 
-module.exports = app;
+module.exports = app
+
