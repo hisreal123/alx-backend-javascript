ac11b766b012af5a174261512b6a89207d3ca264
hisreal123 <omosgoerge@gmail.com>
2024-12-02 10:53:06 +0100

    fixed eslint

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,20 +1,19 @@
 const http = require('http');
 
-const PORT = 1245
-const HOST = 'localhost'
-const app = http.createServer()
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
 
 app.on('request', (_, res) => {
   const responseText = 'Hello Holberton School!';
   res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', responseText.length)
+  res.setHeader('Content-Length', responseText.length);
   res.statusCode = 200;
-  res.write(Buffer.from(responseText))
+  res.write(Buffer.from(responseText));
 });
 
 app.listen(1245, HOST, () => {
   console.log(`Server is listening on -> http://${HOST}:${PORT}\n`);
-})
+});
 
-module.exports = app
-
+module.exports = app;
