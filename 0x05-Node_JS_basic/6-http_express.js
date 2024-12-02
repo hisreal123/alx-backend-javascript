4b6cfec74a7558840f79df42ca3094cd9a51afaf
hisreal123 <omosgoerge@gmail.com>
2024-12-02 10:33:46 +0100

    fixed eslint

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,13 +1,12 @@
-const express = require('express') // express is a module in Nodejs
+const express = require('express'); // express is a module in Nodejs
 
 const app = express();
-const PORT = 1245
+const PORT = 1245;
 
 app.get('/', (_, res) => {
-  res.send("Hello Holberton School!")
-})
+  res.send('Hello Holberton School!');
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
-
+  console.log(`Server is running on port ${PORT}`);
+});
